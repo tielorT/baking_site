b27ca55f8096e1bdd0ccbc9d6186c8ec63c60b83
tielorT <111721927+tielorT@users.noreply.github.com>
2022-10-24 02:05:16 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -51,10 +51,10 @@ export default function Home({ data }) {
 }
 
 export async function getServerSideProps() {
-   if(global.window){
+
   const res = await fetch('https://baking-site-rho.vercel.app/api/recipes');
   const data = await res.json();
 
   return { props: { data }}
-   }
+   
 }
